d46a0b409d6e71baa8d5c1c953c61efe7f65f20c
zhaoyijie <zhaoyijie@mob.com>
2024-03-26 16:54:25 +0800

    zookeeper demo1

--- zookeeper-demo/src/test/java/ZookeeperTest.java
+++ zookeeper-demo/src/test/java/ZookeeperTest.java
@@ -28,6 +28,42 @@ public class ZookeeperTest {
     int sessionTimeoutMs = 60 * 1000;
     int maxCloseWaitMs = 15 * 1000;
 
+    //JVM的钩子函数
+    @Test
+    public void shutdownHookTest(){
+        //jvm关掉前执行一些资源释放等的问题
+        //具体场景
+        //程序正常退出
+        //使用System.exit()
+        //终端使用Ctrl+C触发的中断
+        //系统关闭
+        //OutOfMemory宕机
+        //使用Kill pid命令干掉进程（注：在使用kill -9 pid时，是不会被调用的）
+
+        //hook线程需要收到中断信号才能开启，如果没有收到中断信号，就不会执行hook，导致.lock不能删除，比如kill -9命令，所以防止程序重复运行，还需要其他手段。
+        //hook线程也可以做一些资源释放的操作，类似于try-catch-finally，但不要在hook里面运行复杂的程序，这样会导致程序长时间无法退出。
+        //虚拟机退出：JVM会在所有非后台线程关闭后才会退出，如在一个main线程里新产生一个线程，而该线程阻塞了，那么虚拟机不会退出。后台线程会在所有非后台线程退出后都停止任务。
+        //系统调用System.exit(0)：在线程中调用会导致整个系统退出。在调用该代码之后定义代码均不会运行。
+        //JAVA的钩子函数是在jvm正常退出时会调用：
+        //例如在线程上调用System.exit(0)时会调用注册的钩子函数。钩子函数定义：Runtime.getRuntime().addShutdownHook(“定义的线程”);
+        //ps: linux上kill服务进程会调用钩子函数。
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                try {
+                    System.out.println("这个钩子启动");
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("这个钩子退出");
+            }
+        });
+        //这里是线程调用，而非系统调用，所以依旧会触发钩子函数
+        System.exit(0);
+    }
+
     @Test
     public void createPath() throws Exception {
         CuratorFramework client = ZookeeperUtils.getClient(connectString, sessionTimeoutMs, maxCloseWaitMs, namespace, retryPolicy);
@@ -78,6 +114,14 @@ public class ZookeeperTest {
         client.close();
     }
 
+    @Test
+    public void testGet() throws Exception {
+        CuratorFramework client = ZookeeperUtils.getClient(connectString, sessionTimeoutMs, maxCloseWaitMs, namespace, retryPolicy);
+        client.start();
+        byte[] bytes = client.getData().forPath("/event/attribute");
+        System.out.println(new String(bytes));
+        client.close();
+    }
 
     @Test
     public void testSetForVersion() throws Exception {
